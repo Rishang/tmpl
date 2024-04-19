1f1f314c4ef31e81e5fc8110ddc904be10a3ca5e
test <test@test.com>
2024-04-20 00:20:16 +0530

    Add go.mod, .gitignore, and main.go files

---
+++ src/go.mod
@@ -0,0 +1,13 @@
+module gjinja
+
+go 1.21.1
+
+require (
+	github.com/goph/emperror v0.17.1 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
+	github.com/noirbizarre/gonja v0.0.0-20200629003239-4d051fd0be61 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/sirupsen/logrus v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20180904163835-0709b304e793 // indirect
+	golang.org/x/sys v0.0.0-20190222072716-a9d3bda3a223 // indirect
+)
---
+++ src/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+    "encoding/base64"
+    "encoding/json"
+    "flag"
+    "fmt"
+    "github.com/noirbizarre/gonja"
+    "io/ioutil"
+    "os"
+    "os/exec"
+    "path/filepath"
+)
+
+type ConfigItem struct {
+    Type  string `json:"type"`
+    Key   string `json:"key"`
+    Value string `json:"value"`
+}
+
+func main() {
+    path := flag.String("p", "", "Specify the path to the directory where files will be updated.")
+    configFile := flag.String("c", "tmpl.json", "Specify the path to the JSON configuration file. Default is 'tmpl.json'.")
+    file := flag.String("f", "", "Specify the path to a single file to update.")
+    flag.Usage = usage
+    flag.Parse()
+
+    if *file != "" && *path != "" {
+        fmt.Println("Error: -f and -p flags are mutually exclusive.")
+        return
+    }
+
+    configItems, err := readConfig(*configFile)
+    if err != nil {
+        fmt.Println("Error reading config, for cli reference use --help flag:", err)
+        return
+    }
+
+    context := make(map[string]interface{})
+    for _, item := range configItems {
+        value, err := processConfigItem(item)
+        if err != nil {
+            fmt.Printf("Error processing config item '%s': %v\n", item.Key, err)
+            continue
+        }
+        context[item.Key] = value
+    }
+
+    if *file != "" {
+        fmt.Println("Updating:", *file)
+        if err := updateFile(*file, context); err != nil {
+            fmt.Println("Error updating file:", err)
+        }
+    } else if *path != "" {
+        err = filepath.Walk(*path, func(path string, info os.FileInfo, err error) error {
+            if err != nil {
+                return err
+            }
+            if !info.IsDir() {
+                fmt.Println("Updating:", info.Name())
+                err := updateFile(path, context)
+                if err != nil {
+                    fmt.Println("Error updating file:", err)
+                }
+            }
+            return nil
+        })
+
+        if err != nil {
+            fmt.Println("Error processing files:", err)
+        }
+    } else {
+        fmt.Println("Error: Either -f or -p must be specified.")
+        flag.Usage()
+    }
+}
+
+func usage() {
+    fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+    fmt.Println("Gjinja is a CLI tool for replacing content in files based on Jinja2 templates.")
+    fmt.Println("Options:")
+    flag.PrintDefaults()
+    fmt.Println("\nExamples:")
+    fmt.Println("  gjinja -p /path/to/directory -c /path/to/config.json")
+    fmt.Println("  gjinja -f /path/to/file.txt -c /path/to/config.json")
+    fmt.Println("\nThe config file should contain JSON formatted like:")
+    fmt.Println(`  [
+    {"type": "string", "key": "appName", "value": "Gjinja"},
+    {"type": "base64", "key": "secret", "value": "c2VjcmV0"},
+    {"type": "command", "key": "cwd", "value": "pwd"}
+  ]`)
+}
+
+func readConfig(path string) ([]ConfigItem, error) {
+    var items []ConfigItem
+    data, err := ioutil.ReadFile(path)
+    if err != nil {
+        return nil, err
+    }
+    err = json.Unmarshal(data, &items)
+    return items, err
+}
+
+func processConfigItem(item ConfigItem) (interface{}, error) {
+    switch item.Type {
+    case "base64":
+        decodedBytes, err := base64.StdEncoding.DecodeString(item.Value)
+        if err != nil {
+            return nil, err
+        }
+        return string(decodedBytes), nil
+    case "command":
+        return executeCommand(item.Value)
+    default:
+        return item.Value, nil
+    }
+}
+
+func executeCommand(cmd string) (string, error) {
+    out, err := exec.Command("bash", "-c", cmd).CombinedOutput()
+    if err != nil {
+        return "", fmt.Errorf("command execution failed: %v", err)
+    }
+    return string(out), nil
+}
+
+func updateFile(filePath string, context map[string]interface{}) error {
+    content, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        return fmt.Errorf("failed to read file: %v", err)
+    }
+
+    tmpl, err := gonja.FromString(string(content))
+    if err != nil {
+        return fmt.Errorf("failed to parse template: %v", err)
+    }
+
+    rendered, err := tmpl.Execute(gonja.Context(context))
+    if err != nil {
+        return fmt.Errorf("failed to execute template: %v", err)
+    }
+
+    return ioutil.WriteFile(filePath, []byte(rendered), 0644)
+}
